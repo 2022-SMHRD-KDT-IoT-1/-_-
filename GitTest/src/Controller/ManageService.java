8c47cb662132bd47692ed21a91d5ce5fee93b089
SMHRD <SMHRD@DESKTOP-0HARFDN>
2022-02-17 16:08:00 +0900

    커밋

--- GitTest/src/Controller/ManageService.java
+++ GitTest/src/Controller/ManageService.java
@@ -7,18 +7,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ManageService
- */
 @WebServlet("/ManageService")
 public class ManageService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 * 회원 관리 페이지 컨트롤러
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		System.out.println("짜잔");
 	}
 
 }
