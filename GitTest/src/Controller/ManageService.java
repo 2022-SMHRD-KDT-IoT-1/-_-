4aa8aa7b995689ab5d81795f37060aa744ab3031
SMHRD <SMHRD@DESKTOP-P4BAIKL>
2022-02-18 16:14:11 +0900

    준팡이 추가함

--- GitTest/src/Controller/ManageService.java
+++ GitTest/src/Controller/ManageService.java
@@ -14,6 +14,7 @@ public class ManageService extends HttpServlet {
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("준비된인재영");
+		System.out.println("준팡이");
 	}
 
 }
